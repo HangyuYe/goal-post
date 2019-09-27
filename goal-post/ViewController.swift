c77f58779858dcf97ac0056ea5210c85839ed63c
Hangyu Ye <jerry.hangyu@gmail.com>
2019-09-27 11:36:51 +0800

    Initial Commit

---
+++ goal-post/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goal-post
+//
+//  Created by Jerry on 9/27/19.
+//  Copyright © 2019 SoftChain Foundation Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
