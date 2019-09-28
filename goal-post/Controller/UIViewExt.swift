a8c2c7bbec87422d7ba7859f91abcc1171ab9e56
Hangyu Ye <jerry.hangyu@gmail.com>
2019-09-28 23:37:52 +0800

    Create some extenstion
    1. Bind keyboard using UIResponder
    2. Switch button background when pressed

---
+++ goal-post/Controller/UIViewExt.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewExt.swift
+//  goal-post
+//
+//  Created by Jerry on 2019/9/28.
+//  Copyright © 2019 SoftChain Foundation Ltd. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+        let startingFrame = (notification.userInfo![UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endingFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = endingFrame.origin.y - startingFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0, options: UIView.KeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+        }, completion: nil)
+    }
+}
