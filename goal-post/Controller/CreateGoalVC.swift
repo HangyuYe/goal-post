8e95b50f5b7ee432fde8f7d9143981e87c46c274
Hangyu Ye <jerry.hangyu@gmail.com>
2019-09-28 22:58:19 +0800

    Creation VC create

---
+++ goal-post/Controller/CreateGoalVC.swift
@@ -0,0 +1,40 @@
+//
+//  CreateGoalVC.swift
+//  goal-post
+//
+//  Created by Jerry on 2019/9/28.
+//  Copyright © 2019 SoftChain Foundation Ltd. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
